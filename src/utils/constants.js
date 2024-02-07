0b8a1b55b923b579d2f08471e6a31be1110688ee
Prince5632 <psr8084@gmail.com>
2024-02-07 16:22:21 +0530

    Get data from NowPLayingMovie API from TMDB

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,13 @@
 export const LOGO_URI="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+
+
+export const API_OPTIONS={
+    
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTA0ZGQ0ZmRiMGIxZTEwMmM2MzNkNDMyN2Y3MDU4ZSIsInN1YiI6IjYzNzczZTc0NWIyZjQ3MDA3ZmQ1ZjMzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6L4buk6Q6bC2mNDFVXR4i52JXcAqjTbG-9Aq2MSODXA'
+        }
+      };
+      
